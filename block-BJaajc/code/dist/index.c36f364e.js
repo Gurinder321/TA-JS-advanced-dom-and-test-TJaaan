7b88a3b3e34f779a0d0f63b5d5fd5a57c2321c30
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-03-06 21:34:19 -0800

    Not done

---
+++ block-BJaajc/code/dist/index.c36f364e.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+module.exports = add;
+
+//# sourceMappingURL=index.c36f364e.js.map
--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -1,5 +1,11 @@
-const add = require('./index');
+import add from './index';
 
-test('add 1 + 2 to equal 3', () => {
-  expect(add(1, 2).toBe(3));
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+test('adds 1 + 20 to equal 21', () => {
+  expect(add(1, 20)).toBe(21);
+});
+test('adds 10 + 20 to not equal 21', () => {
+  expect(add(10, 20)).not.toBe(21);
 });
